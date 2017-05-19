b1680542fa49aa2ab107e89600e498aeabac1641
starvi820 <starvi820@gmail.com>
2017-05-19 16:33:37 +0900

    sangjin dd

--- GitTest/src/test/ckdgml.java
+++ GitTest/src/test/ckdgml.java
@@ -4,6 +4,12 @@ public class ckdgml {
 	
 	public static void main(String[] args) {
 		System.out.println("ajdcndajcnd");
+		System.out.println("hihihihihihihi");
+		System.out.println("hihihihihihihi");
+		System.out.println("hihihihihihihi");
+		System.out.println("hihihihihihihi");
+		System.out.println("hihihihihihihi");
+		
 	}
 
 }
